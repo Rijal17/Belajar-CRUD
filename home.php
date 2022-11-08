3b3a624e76d684de9cd409d8f29080c76ec6f17b
Rijal17 <syamsulrijal276@gmail.com>
2022-11-07 20:28:21 -0800

    Menghapus isi dari home.php

--- home.php
+++ home.php
@@ -89,18 +89,6 @@ if (isset($_POST["simpan"])) {
                         </div>
                     </div>
             </main>
-            <footer class="py-4 bg-light mt-auto">
-                <div class="container-fluid">
-                    <div class="d-flex align-items-center justify-content-between small">
-                        <div class="text-muted">Copyright &copy; Your Website 2022</div>
-                        <div>
-                            <a href="#">Privacy Policy</a>
-                            &middot;
-                            <a href="#">Terms &amp; Conditions</a>
-                        </div>
-                    </div>
-                </div>
-            </footer>
         </div>
 </body>
 </html>
