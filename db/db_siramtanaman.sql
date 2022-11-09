93d1d0cebe3213e76ec991480376ad5600657b04
Rijal17 <syamsulrijal276@gmail.com>
2022-11-09 12:26:35 -0800

    Mengganti file database db_siramtanaman

--- db/db_siramtanaman.sql
+++ db/db_siramtanaman.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 04, 2022 at 09:09 AM
+-- Generation Time: Nov 09, 2022 at 09:21 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.0.15
 
@@ -30,17 +30,17 @@ SET time_zone = "+00:00";
 CREATE TABLE `mid_test` (
   `id` int(11) NOT NULL,
   `nama` varchar(200) NOT NULL,
-  `nim` int(11) NOT NULL,
-  `waktu` varchar(200) NOT NULL
+  `jenis_tanaman` varchar(255) NOT NULL,
+  `kelembapan` double NOT NULL,
+  `waktu` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `mid_test`
 --
 
-INSERT INTO `mid_test` (`id`, `nama`, `nim`, `waktu`) VALUES
-(8, 'sdsd', 1112, '1120225'),
-(9, 'sdsd', 1112, '1120225');
+INSERT INTO `mid_test` (`id`, `nama`, `jenis_tanaman`, `kelembapan`, `waktu`) VALUES
+(13, 'rijal', 'Bunga Anggrek', 55.77, '2022-11-09 12:17:41');
 
 --
 -- Indexes for dumped tables
@@ -60,7 +60,7 @@ ALTER TABLE `mid_test`
 -- AUTO_INCREMENT for table `mid_test`
 --
 ALTER TABLE `mid_test`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
